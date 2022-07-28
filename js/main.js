dab54fefeeeb30454e6cbe46984b9c921766b6c1
DZMTRK <5286082@gmail.com>
2022-07-28 16:41:15 +0300

    js interactivity implementation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 "use strict"
+
+let eventTarget = document.getElementById('fontIncrease');
+
+eventTarget.addEventListener("click", IncreaseFontSize);
+
+const IncreaseFontSize = () => {
+    alert(eventTarget);
+};
+
